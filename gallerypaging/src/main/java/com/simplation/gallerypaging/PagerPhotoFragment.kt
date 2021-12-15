0d5b7b7b1e2201ae506670c6f3c4f836d682ecc3
Simplation <simplationwang@gmail.com>
2021-12-15 18:10:05 +0800

    Update PagerPhotoFragment.kt

--- gallerypaging/src/main/java/com/simplation/gallerypaging/PagerPhotoFragment.kt
+++ gallerypaging/src/main/java/com/simplation/gallerypaging/PagerPhotoFragment.kt
@@ -17,7 +17,6 @@ import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
@@ -28,101 +27,102 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class PagerPhotoFragment : Fragment() {
-    private val galleryViewModel: GalleryViewModel by activityViewModels()
-    lateinit var pagerPhotoBinding: FragmentPagerPhotoBinding
+	private val galleryViewModel: GalleryViewModel by activityViewModels()
+	lateinit var pagerPhotoBinding: FragmentPagerPhotoBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        pagerPhotoBinding = FragmentPagerPhotoBinding.inflate(layoutInflater, container, false)
-        return pagerPhotoBinding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		pagerPhotoBinding = FragmentPagerPhotoBinding.inflate(layoutInflater, container, false)
+		return pagerPhotoBinding.root
+	}
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        val adapter = PagerPhotoListAdapter()
-        pagerPhotoBinding.viewPager2.adapter = adapter
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
 
-        galleryViewModel.pageListLiveData.observe(viewLifecycleOwner, {
-            adapter.submitList(it)
-            // 设置点击跳转的值
-            /**
-             * 参数 1: 当前点击的 item
-             * 参数 2: 滑动的动画
-             */
-            pagerPhotoBinding.viewPager2.setCurrentItem(
-                arguments?.getInt("PHOTO_POSITION") ?: 0,
-                false
-            )
-        })
+		val adapter = PagerPhotoListAdapter()
+		pagerPhotoBinding.viewPager2.adapter = adapter
 
-        pagerPhotoBinding.viewPager2.registerOnPageChangeCallback(object :
-            ViewPager2.OnPageChangeCallback() {
-            override fun onPageSelected(position: Int) {
-                super.onPageSelected(position)
-                pagerPhotoBinding.photoTag.text =
-                    resources.getString(
-                        R.string.photo_tag,
-                        position + 1,
-                        galleryViewModel.pageListLiveData.value?.size
-                    )
-            }
-        })
+		galleryViewModel.pageListLiveData.observe(viewLifecycleOwner, {
+			adapter.submitList(it)
+			// 设置点击跳转的值
+			/**
+			 * 参数 1: 当前点击的 item
+			 * 参数 2: 滑动的动画
+			 */
+			pagerPhotoBinding.viewPager2.setCurrentItem(
+				arguments?.getInt("PHOTO_POSITION") ?: 0,
+				false
+			)
+		})
 
-        // 设置 viewPager2 为垂直方向
-        // pagerPhotoBinding.viewPager2.orientation = ViewPager2.ORIENTATION_VERTICAL
+		pagerPhotoBinding.viewPager2.registerOnPageChangeCallback(object :
+			ViewPager2.OnPageChangeCallback() {
+			override fun onPageSelected(position: Int) {
+				super.onPageSelected(position)
+				pagerPhotoBinding.photoTag.text =
+					resources.getString(
+						R.string.photo_tag,
+						position + 1,
+						galleryViewModel.pageListLiveData.value?.size
+					)
+			}
+		})
 
-        pagerPhotoBinding.saveButton.setOnClickListener {
-            if (Build.VERSION.SDK_INT < 29 && ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // registerForActivityResult()
-                requestPermissions(
-                    arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    PERMISSION_REQUEST_CODE
-                )
+		// 设置 viewPager2 为垂直方向
+		// pagerPhotoBinding.viewPager2.orientation = ViewPager2.ORIENTATION_VERTICAL
 
-            } else {
-                viewLifecycleOwner.lifecycleScope.launch {
-                    savePhoto()
-                }
-            }
-        }
-    }
+		pagerPhotoBinding.saveButton.setOnClickListener {
+			if (Build.VERSION.SDK_INT < 29 && ContextCompat.checkSelfPermission(
+					requireContext(),
+					Manifest.permission.WRITE_EXTERNAL_STORAGE
+				) != PackageManager.PERMISSION_GRANTED
+			) {
+				// registerForActivityResult()
+				requestPermissions(
+					arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+					PERMISSION_REQUEST_CODE
+				)
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+			} else {
+				viewLifecycleOwner.lifecycleScope.launch {
+					savePhoto()
+				}
+			}
+		}
+	}
 
-        when (requestCode) {
-            PERMISSION_REQUEST_CODE -> {
-                if (grantResults != null && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    viewLifecycleOwner.lifecycleScope.launch {
-                        savePhoto()
-                    }
-                } else {
-                    Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray
+	) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+		when (requestCode) {
+			PERMISSION_REQUEST_CODE -> {
+				if (grantResults != null && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					viewLifecycleOwner.lifecycleScope.launch {
+						savePhoto()
+					}
+				} else {
+					Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+				}
+			}
+		}
+	}
 
 
-    /*private fun savePhoto() {
-        val holder =
-            (pagerPhotoBinding.viewPager2[0] as RecyclerView).findViewHolderForAdapterPosition(
-                pagerPhotoBinding.viewPager2.currentItem
-            ) as PagerPhotoListAdapter.PagerPhotoListViewHolder
-        val bitmap = holder.itemView.findViewById<ImageView>(R.id.pagerPhoto).drawable.toBitmap()
+	/*private fun savePhoto() {
+		val holder =
+			(pagerPhotoBinding.viewPager2[0] as RecyclerView).findViewHolderForAdapterPosition(
+				pagerPhotoBinding.viewPager2.currentItem
+			) as PagerPhotoListAdapter.PagerPhotoListViewHolder
+		val bitmap = holder.itemView.findViewById<ImageView>(R.id.pagerPhoto).drawable.toBitmap()
 
-        // API 29 以前的写法
-        *//*if (MediaStore.Images.Media.insertImage(
+		// API 29 以前的写法
+		*//*if (MediaStore.Images.Media.insertImage(
                 requireContext().contentResolver, bitmap, "", ""
             ) == null
         ) {
@@ -147,50 +147,50 @@ class PagerPhotoFragment : Fragment() {
         }
     }*/
 
-    // 以上方法的改造
-    private suspend fun savePhoto() {
-        withContext(Dispatchers.IO) {
-            val holder =
-                (pagerPhotoBinding.viewPager2[0] as RecyclerView).findViewHolderForAdapterPosition(
-                    pagerPhotoBinding.viewPager2.currentItem
-                ) as PagerPhotoListAdapter.PagerPhotoListViewHolder
-            val bitmap =
-                holder.itemView.findViewById<ImageView>(R.id.pagerPhoto).drawable.toBitmap()
+	// 以上方法的改造
+	private suspend fun savePhoto() {
+		withContext(Dispatchers.IO) {
+			val holder =
+				(pagerPhotoBinding.viewPager2[0] as RecyclerView).findViewHolderForAdapterPosition(
+					pagerPhotoBinding.viewPager2.currentItem
+				) as PagerPhotoListAdapter.PagerPhotoListViewHolder
+			val bitmap =
+				holder.itemView.findViewById<ImageView>(R.id.pagerPhoto).drawable.toBitmap()
 
-            // API 29 以前的写法
-            if (MediaStore.Images.Media.insertImage(
-                    requireContext().contentResolver, bitmap, "", ""
-                ) == null
-            ) {
-                MainScope().launch {
-                    Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                MainScope().launch {
-                    Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
-                }
-            }
+			// API 29 以前的写法
+			if (MediaStore.Images.Media.insertImage(
+					requireContext().contentResolver, bitmap, "", ""
+				) == null
+			) {
+				MainScope().launch {
+					Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+				}
+			} else {
+				MainScope().launch {
+					Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
+				}
+			}
 
-            val saveUri = requireContext().contentResolver.insert(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                ContentValues()
-            ) ?: kotlin.run {
-                return@withContext
-            }
+			val saveUri = requireContext().contentResolver.insert(
+				MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+				ContentValues()
+			) ?: kotlin.run {
+				return@withContext
+			}
 
-            requireContext().contentResolver.openOutputStream(saveUri).use {
-                // user{}  不需要手动关闭
-                // 需要放在工作线程中
-                if (bitmap.compress(Bitmap.CompressFormat.JPEG, 90, it)) {
-                    MainScope().launch {
-                        Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    MainScope().launch {
-                        Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-    }
+			requireContext().contentResolver.openOutputStream(saveUri).use {
+				// user{}  不需要手动关闭
+				// 需要放在工作线程中
+				if (bitmap.compress(Bitmap.CompressFormat.JPEG, 90, it)) {
+					MainScope().launch {
+						Toast.makeText(requireContext(), "存储成功", Toast.LENGTH_SHORT).show()
+					}
+				} else {
+					MainScope().launch {
+						Toast.makeText(requireContext(), "存储失败", Toast.LENGTH_SHORT).show()
+					}
+				}
+			}
+		}
+	}
 }
